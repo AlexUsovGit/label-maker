89604dd5515bc32533e2c3a51a7a371d3eec10d9
Alexey Usov <alexey.usov@onde.app>
2023-10-06 21:32:13 +0300

    Initial commit

---
+++ src/main/java/com/uaa/demo/demo/LabelMakerApplication.java
@@ -0,0 +1,21 @@
+package com.uaa.demo.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelMakerApplication
+
+{
+
+    public static void main(String[] args)
+    {
+        Logger logger = LoggerFactory.getLogger(LabelMakerApplication.class);
+
+        SpringApplication.run(LabelMakerApplication.class, args);
+        logger.info("\n\n\nLABEL MAKER STARTED\n\n\n");
+    }
+
+}
